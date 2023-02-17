c102ed9ae10972fca913dd4fac1eb19b56b11774
Emlyn O'Regan <emlyn.oregan@healthstream.com>
2023-02-17 14:35:29 +1030

    Supports private and public users

--- tmw.py
+++ tmw.py
@@ -23,6 +23,10 @@ class UnauthorizedError(Exception):
 
     def get_auth_url(self, tenant_id):
         return C_AUTH_URL_TEMPLATE.format(tenant_id=tenant_id)
+    
+    def get_sign_up_url(self):
+        public_invite_url = st.secrets["PUBLIC_INVITE_URL"]
+        return public_invite_url
 
 def tmwcheck():
     # get the querystring param "access_token" if it exists
@@ -33,63 +37,37 @@ def tmwcheck():
     if access_token == "None":
         access_token = None
 
-    # st.write(access_token)
-    # st.write(type(access_token))
-    # raise Exception("xxx")
-
-    #** I removed all storage of the access_token in the state or cookies because all methods either don't
-    # survive a refresh or are global to all users. I'm just leaving the token in the querystring for now.
-
-    # # if no access_token, then get access_token from the state
-    # if not access_token:
-    #     st.write('looking in cookies')
-    #     # st.write(st.session_state)
-    #     # access_token = st.session_state.get("access_token")
-    #     access_token = cookies.get("access_token")
-    #     st.write(access_token)
-    # else:
-    #     # remove the access_token from the querystring
-    #     # st.write(access_token)
-    #     st.experimental_set_query_params(access_token=None)
-
     if not access_token:
         raise UnauthorizedError("You must login to https://themetalwizard.net to use this app. (1)")
-        # # webbrowser.open(C_METALWIZARD_URL)
-        # raise Exception("You must login to https://themetalwizard.net to use this app. (1)")
 
-    # # save the token in the state
-    # # st.session_state["access_token"] = access_token
-    # cookies["access_token"] = access_token
-    # cookies.save()
-
-    # st.write(access_token)
-
-    # get the user info    
     r = requests.get(C_API_URL, headers={"Authorization": f"Bearer {access_token}"})
 
     if r.status_code != 200:
         raise UnauthorizedError("You must login to https://themetalwizard.net to use this app. (2)")
-        # # st.write(f"Error: {r.status_code} {r.text}")
-        # # webbrowser.open(C_METALWIZARD_URL)
-        # raise Exception("You must login to https://themetalwizard.net to use this app. (2)")
     
     response_json = r.json()
-    
-    # st.write(response_json)
 
-    expected_tenant_id = st.secrets["TENANT_ID"]
-
-    tenants = (response_json.get("info") or {}).get("tenants") or {}
-
-    if expected_tenant_id not in tenants:
-        raise UnauthorizedError("You must login to https://themetalwizard.net to use this app. (3)")
-        # # webbrowser.open(C_METALWIZARD_URL)
-        # raise Exception("You must login to https://themetalwizard.net to use this app. (3)")
-
-    # here we're good.
+    st.write(response_json)
 
     return response_json
-    
 
+def tmwcheck_tenant(tenant_id, response_json):
+    tenants = (response_json.get("info") or {}).get("tenants") or {}
+
+    if tenant_id not in tenants:
+        raise UnauthorizedError("You must login to https://themetalwizard.net to use this app. (3)")
+
+    return response_json
+
+def has_scope(tenant_id, scope, response_json):
+
+    scopes = (((response_json.get("info") or {}).get("tenants") or {}).get(tenant_id) or {}).get("scopes") or []
+
+    return scope in scopes
     
+def check_scope(tenant_id, scope, response_json):
+    if not has_scope(tenant_id, scope, response_json):
+        raise UnauthorizedError("You must login to https://themetalwizard.net to use this app. (4)")
+
+    return response_json    
 
--- ytsummary.py
+++ ytsummary.py
@@ -7,11 +7,11 @@ import os
 import json
 import openai
 import streamlit as st
-from tmw import tmwcheck, UnauthorizedError
+from tmw import tmwcheck, tmwcheck_tenant, UnauthorizedError, has_scope, check_scope
+from streamlit_cookies_manager import EncryptedCookieManager
+
 
 # OpenAI API Key
-openai.api_key = st.secrets["OPENAIKEY"]
-
 def get_video_id_from_video_id_or_url(video_id_or_url):
     # a youtube video id is 11 characters long
     # if the video id is longer than that, then it's a url
@@ -33,57 +33,106 @@ def get_video_id_from_video_id_or_url(video_id_or_url):
 def main():
     tenant_id = st.secrets["TENANT_ID"]
 
+    authorized = False
+    unauth_error = None
     try:
         info = tmwcheck()
+        # tmwcheck_tenant(tenant_id, info)
+        check_scope(tenant_id, "public", info)
+        authorized = True
     except UnauthorizedError as e:
-        auth_url = e.get_auth_url(tenant_id)
+        unauth_error = e
 
-        st.write(f"Please click the link below to sign in.")
+    if not authorized:
+        # the user is not authorized
 
-        # add a link that will open the auth url in a new tab
-        st.markdown(f"[Sign in]({auth_url})")
+        auth_url = unauth_error.get_auth_url(tenant_id)
+
+        public_signup_url = unauth_error.get_sign_up_url()
+
+        st.title ("Youtube Video Summarizer")
+
+        st.write ("You must sign in to use this app.")
+
+        st.markdown(f"Click here to [Sign in]({auth_url})")
+        st.markdown(f"Click here to [Sign up]({public_signup_url})")
 
         return
     
     # here we know the user is authorized
-
     user_name = (info.get('user') or {}).get('user_name')
+    user_id = (info.get('user') or {}).get('user_id')
 
     st.title ("Youtube Video Summarizer")
     st.write(f"Hello {user_name}!")
 
-    video_id_or_url = st.text_input("Enter the url or id of the video to summarize")
 
-    chunk_len_mins = st.number_input("Enter the length of the chunks to summarize in minutes", min_value=1, max_value=60, value=10)
+    is_private = has_scope(tenant_id, "private", info)
 
-    if video_id_or_url:
-        video_id = get_video_id_from_video_id_or_url(video_id_or_url)
-        
-        # st.write(f"video_id: {video_id}")
+    api_key = None
+    cookies = EncryptedCookieManager(
+        prefix = f"{user_id}/scm",
+        password = st.secrets["COOKIES_PASSWORD"]
+    )
 
-        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=("en", "en-US", "en-GB"))
+    if not cookies.ready:
+        st.stop()
 
-        # show some kind of progress bar
-        progress = 0
-        p = st.progress(progress)
+    # users that are not private need to provide openai keys
+    if is_private:
+        api_key = st.secrets["OPENAIKEY"]
+    else:
+        api_key = cookies.get("openaikey")
 
-        chunks = get_chunks_from_transcript(transcript, chunk_len_mins)
+    if not api_key:
+        api_key = st.text_input("Enter your OpenAI API Key")
 
-        # update the progress bar
-        progress += 5
-        p.progress(progress)
+        if api_key:
+            cookies["openaikey"] = api_key
+            cookies.save()
 
-        # one progress step should be 90/len(chunks).
-        progress_step = 90 / (len(chunks) + 1)
+    if not api_key:
+        st.stop()
 
-        # summarize_audio_transcript_chunks yields chunks. Get each one and write it.
-        for chunk in summarize_audio_transcript_chunks(chunks, "", chunk_len_mins):
-            st.write(chunk)
-            progress += progress_step
-            progress = int(progress)
+    try:        
+        openai.api_key = api_key
+
+        video_id_or_url = st.text_input("Enter the url or id of the video to summarize")
+
+        chunk_len_mins = st.number_input("Enter the length of the chunks to summarize in minutes", min_value=1, max_value=60, value=10)
+
+        if video_id_or_url:
+            video_id = get_video_id_from_video_id_or_url(video_id_or_url)
+            
+            # st.write(f"video_id: {video_id}")
+
+            transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=("en", "en-US", "en-GB"))
+
+            # show some kind of progress bar
+            progress = 0
+            p = st.progress(progress)
+
+            chunks = get_chunks_from_transcript(transcript, chunk_len_mins)
+
+            # update the progress bar
+            progress += 5
             p.progress(progress)
 
-        p.progress(100)
+            # one progress step should be 90/len(chunks).
+            progress_step = 90 / (len(chunks) + 1)
+
+            # summarize_audio_transcript_chunks yields chunks. Get each one and write it.
+            for chunk in summarize_audio_transcript_chunks(chunks, "", chunk_len_mins):
+                st.write(chunk)
+                progress += progress_step
+                progress = int(progress)
+                p.progress(progress)
+
+            p.progress(100)
+    except openai.error.AuthenticationError as e:
+        cookies["openaikey"] = ""
+        cookies.save()
+        raise e
 
 if __name__ == "__main__":
     main()
